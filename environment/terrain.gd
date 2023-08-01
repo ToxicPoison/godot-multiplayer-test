d8301d0af841847fe7c19ffa9daac0ae11bbda33
Kyle <kyle.jon.carlson@gmail.com>
2023-08-01 13:06:17 -0700

    Added multiplayer
    Living up to its name!

---
+++ environment/terrain.gd
@@ -0,0 +1,11 @@
+extends TileMap
+
+
+var width : int = 32
+var height : int = 16
+
+func generate_terrain():
+	pass
+
+func _ready():
+	generate_terrain()
---
+++ player/player.gd
@@ -0,0 +1,37 @@
+extends CharacterBody2D
+
+
+const SPEED = 100.0
+const JUMP_VELOCITY = -200.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _enter_tree():
+	set_multiplayer_authority(str(name).to_int())
+
+func _ready():
+	if not is_multiplayer_authority(): return
+	$Camera2D.make_current()
+
+
+func _physics_process(delta):
+	if not is_multiplayer_authority(): return
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
